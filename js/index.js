104e068fea3b9aeda512585b0234cb2be34cd592
Iván <ivangeorgel1998@gmail.com>
2024-04-20 15:47:48 -0300

    Primera copia

---
+++ js/index.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const slides = document.querySelectorAll('.background-slide');
+    let currentSlide = 0;
+  
+    function showSlide(n) {
+      slides[currentSlide].classList.remove('active');
+      currentSlide = (n + slides.length) % slides.length;
+      slides[currentSlide].classList.add('active');
+    }
+  
+    document.getElementById('nextBtn').addEventListener('click', function() {
+      showSlide(currentSlide + 1);
+    });
+  });
+
+
+  const barraNavegacion = document.getElementById('barraNavegacion');
+
+  barraNavegacion.addEventListener('mouseover', function() {
+    this.style.backgroundColor = 'hsl(75, 94%, 57%)';
+  });
